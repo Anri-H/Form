844dd235daf23e781b81433d084509a3daff797b
Anri <anrihunanyan@gmail.com>
2021-07-17 02:27:40 +0400

    feat(validation.js): created last & first names validation func

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -3,6 +3,13 @@ export const validation = {
     const reg = /^\S+@\S+\.\S+$/;
     return !reg.test(val);
   },
-  
+  firstName: (val) => {
+    const reg = /^[a-z ,.'-]+$/;
+    return !reg.test(val);
+  },
+  lastName: (val) => {
+    const reg = /^[a-z ,.'-]+$/;
+    return !reg.test(val);
+  },
 };
 

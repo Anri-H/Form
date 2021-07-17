49ce48733ba4c568e9f7cbc488f5e72905a4078c
Anri <anrihunanyan@gmail.com>
2021-07-17 04:09:09 +0400

    feat(validation.js): created password validation

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -4,12 +4,16 @@ export const validation = {
     return !reg.test(val);
   },
   firstName: (val) => {
-    const reg = /^[a-z ,.'-]+$/;
+    const reg = /[a-zA-Z]{1,}/;
     return !reg.test(val);
   },
   lastName: (val) => {
-    const reg = /^[a-z ,.'-]+$/;
+    const reg = /[a-zA-Z]{1,}/;
+    return !reg.test(val);
+  },
+  password: (val) => {
+    const reg =
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     return !reg.test(val);
   },
 };
-

2b5051bd560b81bbe1fc2911e1141d9fe0038698
Anri <anrihunanyan@gmail.com>
2021-07-17 04:36:47 +0400

    feat(input.jsx): added password invalid message

--- src/Components/Input/Input.jsx
+++ src/Components/Input/Input.jsx
@@ -1,6 +1,13 @@
 import style from "./input.module.css";
 
-export default function Input({title, type, name, onChange, disabled  }) {
+export default function Input({
+  title,
+  type,
+  name,
+  value,
+  onChange,
+  disabled,
+}) {
   return (
     <div className={style.labelGroup}>
       <h4 className={style.title}>{title}</h4>
@@ -8,10 +15,22 @@ export default function Input({title, type, name, onChange, disabled  }) {
         className={style.Input}
         type={type}
         name={name}
+        value={value}
         onChange={onChange}
       />
+
       {disabled ? (
-        <p className={style.inValid}>Please enter some value</p>
+        name === "password" ? (
+          <>
+            <p className={style.inValid}>Minimum eight characters</p>
+            <p className={style.inValid}>At least one uppercase letter</p> 
+            <p className={style.inValid}>One lowercase letter</p>
+            <p className={style.inValid}>One number</p> 
+            <p className={style.inValid}>One special character</p>
+            </>
+        ) : (
+          <p className={style.inValid}>Your input invalid</p>
+        )
       ) : (
         ""
       )}
